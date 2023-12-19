13c547bbb491db536123ac5d87eb2998de3d6327
Piotr Przetacki <pprzetacki@tm1.edu.pl>
2023-12-19 09:02:16 +0100

    Read grades

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 oceny = open('oceny.txt', 'r')
 oceny.readline()
-    
+oceny_uczniow = {}
+for ocena in oceny:
+    o = ocena.split(';')
+    id = o[0]
+    ocena = o[1]
+    if id in oceny_uczniow.keys():
+        oceny_uczniow[id].append(ocena)
+    else:
+        oceny_uczniow[id] = [ocena]
+
+print(oceny_uczniow)
