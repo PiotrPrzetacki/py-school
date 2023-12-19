2bdee239338a5c1b6681ebc7eb3a6abad7641622
Piotr Przetacki <pprzetacki@tm1.edu.pl>
2023-12-19 09:10:24 +0100

    Calculate avg

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ oceny.readline()
 oceny_uczniow = {}
 for ocena in oceny:
     o = ocena.split(';')
-    id = o[0]
-    ocena = o[1]
+    id = int(o[0])
+    ocena = int(o[1])
     if id in oceny_uczniow.keys():
         oceny_uczniow[id].append(ocena)
     else:
         oceny_uczniow[id] = [ocena]
 
-print(oceny_uczniow)
+max_avg = {'id': '0', 'avg': 0}
+for id in oceny_uczniow.keys():
+    oceny_ucznia = oceny_uczniow[id]
+    avg = sum(oceny_ucznia) / len(oceny_ucznia)
+    if avg>max_avg['avg']:
+        max_avg['avg'] = avg
+        max_avg['id'] = id
+
+print(max_avg['id'])
